1039847667e2d53094ad58f3b9af8175449dc750
lucasdeandrade <lucasdeandrade.dev@gmail.com>
2023-02-13 00:49:41 -0300

    Adjusted validations

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,7 +3,8 @@ class Client < ApplicationRecord
     VALID_CPF_REGEX = /([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/i
 
     validates :name, :cpf, :email, :phone, presence: true
-    validates :email, :cpf, uniqueness: true
+    validates :email, uniqueness: true
+    validates :cpf, uniqueness: true
     validates :email, format: { with: VALID_EMAIL_REGEX }
     validates :cpf, format: { with: VALID_CPF_REGEX }
 end
